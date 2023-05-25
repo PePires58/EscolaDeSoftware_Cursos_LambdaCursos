e696d89e0898659c573ace2f435ed2b3080c852a
Pedro Henrique Pires <pedrohenriquepires13@gmail.com>
2023-05-25 20:06:31 -0300

    adding test

---
+++ src/specs/send-email-validator.spec.js
@@ -0,0 +1,7 @@
+const assert = require('assert').strict;
+
+describe('Should create a object from item', function () {
+    it('Should return an object', function () {
+        assert.strictEqual(0, 0);
+    });
+});
