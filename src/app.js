7f2bb31e6d21f234fd415c023142c33e8f7bf44b
Pedro Henrique Pires <pedrohenriquepires13@gmail.com>
2023-05-25 20:02:07 -0300

    mudando para nodejs

---
+++ src/app.js
@@ -0,0 +1,49 @@
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
+
+exports.lambdaHandler = async (event, context) => {
+
+    try {
+        const params = {
+            TableName: "tbes_cursos_dev",
+            ConsistentRead: false
+        };
+
+        dynamodb.query(params, function (err, data) {
+            if (err) console.log(err, err.stack);
+            else console.log(data);
+        });
+
+        return defaultResult(200, 'Dados buscados com sucesso');
+
+
+    } catch (error) {
+        return errorResult(500, error);
+    }
+}
+
+function errorResult(statusCode, errors) {
+    return {
+        'statusCode': statusCode,
+        'body': JSON.stringify({
+            errors: errors
+        }),
+        'isBase64Encoded': false,
+        'headers': {
+            'Content-Type': 'application/json'
+        }
+    };
+}
+
+function defaultResult(statusCode, message) {
+    return {
+        'statusCode': statusCode,
+        'body': JSON.stringify({
+            message: message
+        }),
+        'isBase64Encoded': false,
+        'headers': {
+            'Content-Type': 'application/json'
+        }
+    }
+}
