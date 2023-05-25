6062ded78f53c790a0525a1bc4129603f5875d31
Pedro Henrique Pires <pedrohenriquepires13@gmail.com>
2023-05-25 20:10:04 -0300

    update to scan

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ exports.lambdaHandler = async (event, context) => {
             ConsistentRead: false
         };
 
-        dynamodb.query(params, function (err, data) {
+        dynamodb.scan(params, function (err, data) {
             if (err) console.log(err, err.stack);
             else console.log(data);
         });
