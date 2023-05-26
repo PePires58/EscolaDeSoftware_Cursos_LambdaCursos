232e15e76ba398b024060ab815e20246535b028b
Pedro Henrique Pires <pedrohenriquepires13@gmail.com>
2023-05-25 21:20:44 -0300

    adding unit test

--- src/specs/send-email-validator.spec.js
+++
@@ -1,7 +0,0 @@
-const assert = require('assert').strict;
-
-describe('Should create a object from item', function () {
-    it('Should return an object', function () {
-        assert.strictEqual(0, 0);
-    });
-});
